629352cbef545e2b5a53d2df312322d970c78530
QIU Yuan <yqiuac@gmail.com>
2015-11-02 17:55:28 +0800

    test for time filter

---
+++ test/unit/trfilterTest.js
@@ -0,0 +1,109 @@
+'use strict';
+
+var questionList=[{
+  wholeMsg: "newTodo",
+  head: "head",
+  headLastChar: "?",
+  desc: "desc",
+  linkedDesc: "linkedDesc",
+  completed: false,
+  timestamp: 0,
+  tags: "...",
+  echo: 3,
+  order: 3
+},{
+  wholeMsg: "newTodo",
+  head: "head",
+  headLastChar: "?",
+  desc: "desc",
+  linkedDesc: "linkedDesc",
+  completed: false,
+  timestamp: 0,
+  tags: "...",
+  echo: 2,
+  order: 4
+},{
+  wholeMsg: "newTodo",
+  head: "head",
+  headLastChar: "?",
+  desc: "desc",
+  linkedDesc: "linkedDesc",
+  completed: false,
+  timestamp: 0,
+  tags: "...",
+  echo: 2,
+  order: 5
+},{
+  wholeMsg: "newTodo",
+  head: "head",
+  headLastChar: "?",
+  desc: "desc",
+  linkedDesc: "linkedDesc",
+  completed: false,
+  timestamp: 0,
+  tags: "...",
+  echo: 2,
+  order: 6
+},{
+  wholeMsg: "newTodo",
+  head: "head",
+  headLastChar: "?",
+  desc: "desc",
+  linkedDesc: "linkedDesc",
+  completed: false,
+  timestamp: new Date().getTime(), //new
+  tags: "...",
+  echo: 2,
+  order: 0
+},{
+  wholeMsg: "newTodo",
+  head: "head",
+  headLastChar: "?",
+  desc: "desc",
+  linkedDesc: "linkedDesc",
+  completed: false,
+  timestamp: new Date().getTime()-1, //new
+  tags: "...",
+  echo: 0,
+  order: 2
+},{
+  wholeMsg: "newTodo",
+  head: "head",
+  headLastChar: "?",
+  desc: "desc",
+  linkedDesc: "linkedDesc",
+  completed: false,
+  timestamp: new Date().getTime(), // latest
+  tags: "...",
+  echo: 0,
+  order: 1
+}];
+
+describe('TodoCtrl', function() {
+  beforeEach(module('todomvc'));
+
+  describe('timeRangeFilter Testing', function() {
+    beforeEach(module(function($provide) {
+      $provide.value('version', 'TEST_VER'); //TODO: what is this provide?
+      console.log("provide.value: " + $provide.value);
+    }));
+
+    it('has a timeRange filter', inject(function($filter) {
+      expect($filter('trfilter')).not.toBeNull();
+    }));
+
+     it('trfilter Test', inject(function(trfilterFilter) { // need to put Filter suffix
+	 var filteredList = trfilterFilter(questionList, -3600000, 0);
+         expect(filteredList[0]).not.toBeNull();
+     }));
+
+    // it('Filter max test', inject(function(questionFilterFilter) { // need to put Filter suffix
+    //   var filteredList = questionFilterFilter(questionList, 1);
+    //   expect(filteredList.length).toEqual(5);
+
+    //   for (var i in filteredList) {
+    //     expect(""+filteredList[i].order).toEqual(i);
+    //   }
+    // }));
+  });
+});
