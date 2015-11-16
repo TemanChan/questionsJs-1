388008b765f3dff852cf71355661d684ddc0b8b1
TemanChan <chantmq@gmail.com>
2015-11-16 21:34:58 +0800

    add functions to signup and login; only allow logged in users to delete or edit posts/replies

--- js/services/RESTfulAPI.js
+++ js/services/RESTfulAPI.js
@@ -3,8 +3,8 @@
 todomvc.factory("RESTfulAPI", ['$resource', 'socketFactory', function($resource, socketFactory)
 {
     //  var serverURL = 'http://52.76.51.251'; Meluo's API
-    //var serverURL = 'http://54.254.251.203';
-    var serverURL = 'http://54.169.201.112';
+    var serverURL = 'http://54.254.251.203';
+    //var serverURL = 'http://54.169.201.112'; // for testing
 	var baseURL = serverURL + ':5000/api/';
 
 	var api = {};
@@ -28,6 +28,36 @@ todomvc.factory("RESTfulAPI", ['$resource', 'socketFactory', function($resource,
 			}
 		}
 	);
+
+	api.userResource = $resource(baseURL + "users", {}, {
+		signup: { method: "POST", isArray: false, params: {option: 'signup'} },
+		login: { method: "POST", isArray: false, params: {option: 'login'} }
+	});
+
+	// can be called using RESTfulAPI.signup() in controller
+	api.signup = function(username, passwd, callback) {
+		var user = {
+			username: username,
+			password: passwd
+		};
+		api.userResource.signup(user, function(data) {
+			// received data is a dictionary. {"result": boolean}
+			var signupResult = data.result;
+			callback(signupResult);
+		});
+	};
+
+	// can be called using RESTfulAPI.login() in controller
+	api.login = function(username, passwd, callback) {
+		var user = {
+			username: username,
+			password: passwd
+		};
+		api.userResource.login(user, function(data) {
+			var loginResult = data.result;
+			callback(loginResult);
+		});
+	};
 
 	api.posts = [];
 	api.idPostMap = {};
@@ -72,6 +102,8 @@ todomvc.factory("RESTfulAPI", ['$resource', 'socketFactory', function($resource,
 	// 	});
 	// });
 
+	// can be called using RESTfulAPI.postQuery in the controller
+	// to get posts of a user, pass {username: UserNameOfPosts} to params
 	api.postQuery = function(params){
 		api.posts = api.postResource.query(params, function(){
 			api.posts.forEach(function(post){
@@ -88,6 +120,7 @@ todomvc.factory("RESTfulAPI", ['$resource', 'socketFactory', function($resource,
 		return api.posts;
 	};
 
+	// to get replies of a user, pass {username: UserNameOfReplies} to params
 	api.replyQuery = function(params) {
 		return api.replyResource.query(params);
 	};
@@ -97,23 +130,27 @@ todomvc.factory("RESTfulAPI", ['$resource', 'socketFactory', function($resource,
 			post.reply = [];
 			api.posts.splice(0, 0, post);
 			api.idPostMap[post._id] = post;
-			api.socket.emit('new post', {id: post._id, room: post.roomName});
+			api.socket.emit('new post', {room: post.roomName, id: post._id});
 		});
 	};
 	
-	api.removePost = function(post) {
-		post.reply.forEach(function(reply){
-			api.replyResource.remove({id: reply._id});
+	// can be called using RESTfulAPI.removePost in the controller
+	// call this function only after the user have logged in
+	api.removePost = function(post, currentUsername) {
+		return api.postResource.remove({id: post._id, username: currentUsername}, function(data) {
+			if(data.result == true) {
+				// inform other clients to delete the post
+				api.socket.emit('del post', {room: post.roomName, id: post._id});
+			}
 		});
-		//api.posts.splice(api.posts.indexOf(post), 1);
-		api.socket.emit('del post', {room: post.roomName, id: post._id});
-		return api.postResource.remove({id: post._id});
 	};
 	
-	api.updatePost = function(post) {
+	// can be called using RESTfulAPI.updatePost in the controller
+	// call this function only after the user have logged in
+	api.updatePost = function(post, currentUsername) {
 	    // todo: update the post in the local api.posts first
 	    // update the post on the server
-	    return api.postResource.update({id: post._id}, post);
+	    return api.postResource.update({id: post._id, username: currentUsername}, post);
 	};
 
 	api.likePost = function(post) {
@@ -130,9 +167,22 @@ todomvc.factory("RESTfulAPI", ['$resource', 'socketFactory', function($resource,
 		});
 	};
 
-	api.removeReply = function(post, reply) {
-		post.reply.splice(post.reply.IndexOf(reply), 1);
-		return api.replyResource.remove({id: reply._id});
+	// can be called using RESTfulAPI.removeReply in the controller
+	// call this function only after the user have logged in
+	api.removeReply = function(post, reply, currentUsername) {
+		return api.replyResource.remove({id: reply._id, username: currentUsername}, function(data) {
+			if(data.result == true) {
+				post.reply.splice(post.reply.IndexOf(reply), 1);
+			}
+		});
+	};
+
+	// can be called using RESTfulAPI.updateReply in the controller
+	// call this function only after the user have logged in
+	api.updateReply = function(reply, currentUsername) {
+	    // todo: update the reply in the local storage first
+	    // update the reply on the server
+	    return api.replyResource.update({id: reply._id, username: currentUsername}, reply);
 	};
 
 	return api;
